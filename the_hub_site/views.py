ab959585a3c6ba67faaae13a1d9d72c2ac00b0bc
Radoslav Genov <radoslavgenov5@hotmail.com>
2017-02-20 16:47:16 +0200

    first view function

--- the_hub_site/views.py
+++ the_hub_site/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Events, Member, Projects, Semester
 # Create your views here.
+
+def index(request):
+    events = Events.objects.all()
+    members = Member.objects.all()
+    projects = Projects.objects.all()
+    semesters = Semester.objects.all()
+    context = {'events': events, 'members': members, 'projects': projects, 'semesters': semesters}
+    return render(request, 'the_hub_site/index.html', context)
+
