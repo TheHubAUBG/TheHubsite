a0b6c97149b30c48929e3eb5c530f419facaec8e
hi-im-lazo <lazar.sgeorgiev@yahoo.com>
2017-04-11 20:53:51 +0300

    Our Team structure 7/10

--- the_hub_site/static/js/scripts.js
+++ the_hub_site/static/js/scripts.js
@@ -1,4 +1,10 @@
 $( document ).ready(function() {
     $('.avatar-box').css('height', $('.avatar-box').width());
-    $('#team').css('max-height', $('.avatar-box').width()*2)
+    $('.team .row').css('max-height', $('.team .col-xs-6').height()*2+ 20);
+    $('.prev, .next').css('height', $('.team').height());
+    var wwidth = $(window).width();
+    for(var i = 1; i <= $('.team').length; i+= 1){
+        console.log(i);
+        $('.team:nth-of-type('+ i +')').css('left', (wwidth - $('.team:nth-of-type('+ i +')').width())/2 + wwidth * (i-1));
+    }
 });
